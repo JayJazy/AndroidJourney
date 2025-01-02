65e1ea134778503d4b356ec782b253892414fb83
정영석 <0423jys@naver.com>
2025-01-03 03:07:30 +0900

    Design Login & Sign Up UI

---
+++ SNSApp/presentation/src/main/java/com/example/presentation/login/LoginActivity.kt
@@ -0,0 +1,9 @@
+package com.example.presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
+
+class LoginActivity : AppCompatActivity()
+{
+
+}
